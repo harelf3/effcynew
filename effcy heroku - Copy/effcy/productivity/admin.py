6765107e77957aca68f26104f0a1eef3d73852f1
Harel <81562611+harelf3@users.noreply.github.com>
2022-12-15 19:32:36 +0200

    Delete effcy heroku - Copy/effcy directory

--- effcy heroku - Copy/effcy/productivity/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-
-from .models import Connections,NetworkEvents,Data
-
-admin.site.register(Data)
-admin.site.register(Connections)
-admin.site.register(NetworkEvents)
